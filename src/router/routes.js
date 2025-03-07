535ff435fdc27aca0d4e15254214e94a7ecd04bd
Herva17 <herveiragi80@gmail.com>
2025-03-07 11:00:40 +0100

    first commit

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,18 +1,43 @@
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      { path: "", component: () => import("src/pages/IndexPage.vue") },
+      {
+        path: "/inscription",
+        component: () => import("src/components/RegisterCount.vue"),
+      },
+      {
+        path: "/ouvrage",
+        component: () => import("pages/HistoirePage.vue"),
+      },
+      { path: "/histoire", component: () => import("pages/HistoirePage.vue") },
+      {
+        path: "/publier",
+        component: () => import("src/pages/OuvragePage.vue"),
+      },
+      {
+        path: "/connection",
+        component: () => import("pages/LogginPage.vue"),
+      },
+      {
+        path: "/Enregistrer",
+        component: () => import("src/pages/InscriptionPage.vue"),
+      },
+      {
+        path: "/about",
+        component: () => import("src/pages/AboutPage.vue"),
+      },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
